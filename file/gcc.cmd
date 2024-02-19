8a8f1340168e218b35e49535443e2231b82761bd
sky line <32512284+linesky@users.noreply.github.com>
2024-02-19 00:08:04 +0000

    Add files via upload

--- file/gcc.cmd
+++ file/gcc.cmd
@@ -4,6 +4,6 @@
 @del .\tmp\*.o  >.\tmp\null
 @copy %1 .\tmp\kernel.c
 @as.exe .\file\boot.s -o .\tmp\boot.o
-@g++.exe -c -I.\\file .\tmp\kernel.c -o .\tmp\kernel.o
-@ld.exe --image-base 0x00101000 -nostdlib -T  .\file\link.ld .\tmp\boot.o .\tmp\kernel.o  -o .\tmp\hello.bin
+@g++.exe -mconsole -c -I.\\file .\tmp\kernel.c -o .\tmp\kernel.o
+@ld.exe -nostdlib -T  .\file\link.ld .\tmp\boot.o .\tmp\kernel.o  -o .\tmp\hello.bin
 @set PATH=%paths%
