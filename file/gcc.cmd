3afbe3df1353e202a94caa4b95a0d7fe6ede2ebb
sky line <32512284+linesky@users.noreply.github.com>
2024-02-19 03:15:25 +0000

    Delete file/gcc.cmd

--- file/gcc.cmd
+++
@@ -1,9 +0,0 @@
-@set paths=%PATH%
-@set PATH=c:\mingw\bin\;%paths%
-@del .\tmp\*.exe >.\tmp\null
-@del .\tmp\*.o  >.\tmp\null
-@copy %1 .\tmp\kernel.c
-@as.exe .\file\boot.s -o .\tmp\boot.o
-@g++.exe -mconsole -c -I.\\file .\tmp\kernel.c -o .\tmp\kernel.o
-@ld.exe -nostdlib -T  .\file\link.ld .\tmp\boot.o .\tmp\kernel.o  -o .\tmp\hello.bin
-@set PATH=%paths%
